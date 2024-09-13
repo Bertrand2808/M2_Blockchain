1082b71c4855e1c6826d5713d15d2544e8f267e2
Bertrand2808 <bertrand2808@icloud.com>
2024-09-13 09:21:57 +0200

    Fix deployment of Bertrand Token

--- TP/contracts/Bertrand.sol
+++ TP/contracts/Bertrand.sol
@@ -4,8 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract BERTRAND is ERC20 {
-    constructor() ERC20("BERTRAND", "BRT") {}
-    function issueToken() public{
-        _mint(msg.sender, 1000*10**18);
+    constructor() ERC20("BERTRAND", "BRT") {
+        _mint(msg.sender, 10000*10**18);
     }
 }
